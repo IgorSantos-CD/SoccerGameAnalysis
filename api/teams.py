e1704a620895d8929df74e54d4044c30f93b6137
IgorSantos-CD <igor.5antos@outlook.com>
2025-05-13 14:50:06 -0300

    criando a função teams

---
+++ api/teams.py
@@ -0,0 +1,13 @@
+import requests
+
+
+def fetch_teams(headers,competitions):
+    ids = [competition['code'] for competition in competitions]
+    for id in ids:
+        url = f'http://api.football-data.org/v4/competitions/{id}/teams'
+        response = requests.get(url, headers)
+
+        if response.status_code != 200:
+            raise Exception(f'Não foi possivel obter dados dos times {response.status_code}')
+        else:
+            print(response.json())
