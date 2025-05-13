c500c137d2630c703a9a84172ad28ca88d8cc782
IgorSantos-CD <igor.5antos@outlook.com>
2025-05-13 12:09:10 -0300

    Aprimorando o código utilizando funções

--- app.py
+++ app.py
@@ -1,30 +1,12 @@
-import requests
+'''import requests
 from dotenv import load_dotenv
 import os
-import supabase
+import supabase'''
 
-load_dotenv('./.env')
+from Functions.fetchs import *
 
-API_TOKEN = os.getenv("API_TOKEN") #Obtendo chave de API do arquivo .env
-
-#Configurando o header da requisição, isso que vai liberar o acesso à API
-headers ={
-    "X-Auth-Token" : API_TOKEN
-}
-
-url = "https://api.football-data.org/v4/competitions/"
-
-response = requests.get(url=url, headers=headers)
-
-if response.status_code == 200:
-    print("Requisição bem-sucedida")
-    data = response.json()
-    print(data)
-
-    for i, comp in enumerate(data['competitions']):
-        print(f'{i+1}. {comp['name']} - Area_code: {comp['area']['code']} ({comp['code']})')
-else:
-    print(f'Erro: {response.status_code}')
+competitions = fetch_competitions()
+insert_competitions(competitions)
 
 
 
