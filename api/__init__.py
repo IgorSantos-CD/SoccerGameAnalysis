f80cd91a86287123887d108b4e0ededae78b3a02
IgorSantos-CD <igor.5antos@outlook.com>
2025-05-19 16:50:27 -0300

    novas funções

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,5 @@
 from .competitions import fetch_competitions
 from .seasons import fetch_seasons
-from .teams import fetch_teams
+from .teams import fetch_teams, fetch_teams_by_match
 from .standings import fetch_standings
 from .matches import fetch_matches
