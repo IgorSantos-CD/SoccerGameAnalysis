a0f2ad986a363cf40f9bda598771f551bccbbf98
IgorSantos-CD <igor.5antos@outlook.com>
2025-05-19 18:04:53 -0300

    fatorando página

--- sync_api_to_db.py
+++ sync_api_to_db.py
@@ -1,23 +1,13 @@
 from api import fetch_competitions, fetch_seasons, fetch_teams, fetch_standings, fetch_matches, fetch_teams_by_match
 from db import insert_competitions, insert_seasons, insert_matches, insert_teams
-import os
+from supabase_local import connect_to_supabase, define_headers
 from dotenv import load_dotenv
 from supabase import create_client
 import pandas as pd
 
-load_dotenv('./.env')
+headers = define_headers()
 
-API_TOKEN = os.getenv("API_TOKEN") #Obtendo chave de API do arquivo .env
-
-#Configurando o header da requisição, isso que vai liberar o acesso à API
-headers ={
-    "X-Auth-Token" : API_TOKEN
-}
-
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase = connect_to_supabase()
 
 '''#Competições (competitions)
 competitions = fetch_competitions(headers)
