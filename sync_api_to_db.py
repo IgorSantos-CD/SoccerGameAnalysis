440425958e7e82d9d4e6fb740e9ad1fa934ed229
IgorSantos-CD <igor.5antos@outlook.com>
2025-05-19 16:50:02 -0300

    Buscando jogos e subindo para o banco

--- sync_api_to_db.py
+++ sync_api_to_db.py
@@ -1,5 +1,5 @@
-from api import fetch_competitions, fetch_seasons, fetch_teams, fetch_standings, fetch_matches
-from db import insert_competitions, insert_seasons
+from api import fetch_competitions, fetch_seasons, fetch_teams, fetch_standings, fetch_matches, fetch_teams_by_match
+from db import insert_competitions, insert_seasons, insert_matches, insert_teams
 import os
 from dotenv import load_dotenv
 from supabase import create_client
@@ -30,8 +30,16 @@ seasons = fetch_seasons(headers, competitions.data)
 insert_seasons(supabase, seasons)'''
 
 
-#Matches (jogos)
+'''#Matches (jogos)
 matches = fetch_matches(headers, competitions.data)
+insert_matches(supabase, matches, batch_size=200)
+matches = pd.DataFrame(matches)
+matches.to_csv('./output/matches/matches.csv', index=False)'''
+
+'''# Teams (times)
+teams = fetch_teams_by_match(headers, competitions.data)
+teams = pd.DataFrame(teams).drop_duplicates(subset=['id']).to_dict(orient='records')
+insert_teams(supabase, teams)'''
 
 
 
