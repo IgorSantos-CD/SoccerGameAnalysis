a9e0838c84d8a7e0553fe77a0397c8e56bf913ba
IgorSantos-CD <igor.5antos@outlook.com>
2025-05-22 22:56:51 -0300

    incluindo função no init

--- db/__init__.py
+++ db/__init__.py
@@ -2,3 +2,4 @@ from .insert_competitions import insert_competitions
 from .insert_season import insert_seasons
 from .insert_matches import insert_matches
 from .insert_teams import insert_teams
+from .queries import select_supabase
