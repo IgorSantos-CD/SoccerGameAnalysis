648e7101b979e9001375ad1381c46b594ea25fda
IgorSantos-CD <igor.5antos@outlook.com>
2025-05-26 09:50:01 -0300

    funções de busca de dados (rodadas_disputadas e media_gols)

--- func_streamlit/buscar_dados.py
+++ func_streamlit/buscar_dados.py
@@ -14,4 +14,24 @@ def year_validate(years, liga_id):
     return years_not_null
 
 
+def rodadas_disputadas(competition, season):
+    current_matchday = select_supabase(supabase, 'seasons', 'current_matchday',{'id' : season, 'competition_id' : competition})
+    rodada_atual = current_matchday[0]['current_matchday']
+    return rodada_atual
+
+def media_gols(matches):
+    goals_home = []
+    goals_away = []
+    for matche in matches:
+        home = matche['home_score']
+        away = matche['away_score']
+        goals_home.append(home)
+        goals_away.append(away)
+    goals_total = sum(goals_home) + sum(goals_away)
+    media = round(goals_total/len(matches),2)
+    return media
+
+
+
+
 
