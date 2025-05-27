ff78a544b8843fbcfe20a324ac9f8797ef66374e
IgorSantos-CD <igor.5antos@outlook.com>
2025-05-27 17:32:33 -0300

    ajustando box de apresentação de dados

--- func_streamlit/apresentar.py
+++ func_streamlit/apresentar.py
@@ -4,16 +4,20 @@ import streamlit as st
 # Função para criar o quadro estilizado
 def big_number_card(title, value,color="#000000"):
     st.markdown(f"""
-        <div style='
-            width: 100%;
-            height: 50%;
-            border: 2px solid {color};
-            border-radius: 10px;
-            padding: 20px;
-            background-color: #C0C0C0;
-            box-shadow: 2px 2px 10px rgba(0,0,0,0.1);
-        '>
-            <h4 style='margin-bottom:5px; color: {color}; font-size: 15px'>{title}</h4>
-            <h2 style='margin:0; color: {color}; font-size: 30px''>{value}</h2>
-        </div>
-    """, unsafe_allow_html=True)
+    <div style='
+        width: 80%;
+        height: 50%;
+        border: 2px solid {color};
+        border-radius: 10px;
+        padding: 5px;
+        background-color: #C0C0C0;
+        box-shadow: 2px 2px 10px rgba(0,0,0,0.1);
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    '>
+        <h3 style='margin-bottom:5px; color: {color}; font-size: 15px'>{title}</h3>
+        <h2 style='margin:0; color: {color}; font-size: 30px'>{value}</h2>
+    </div>
+""", unsafe_allow_html=True)
