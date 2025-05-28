43d0919335f81248c989c3d3068ff78b2b711351
IgorSantos-CD <igor.5antos@outlook.com>
2025-05-28 17:59:44 -0300

    Comentários para facilitar a interpretação

--- SoccerAnalysis.py
+++ SoccerAnalysis.py
@@ -49,20 +49,25 @@ st.markdown('---')
 ## BUSCANDO AS PATIDAS DA LIGA E DA TEMPORADA SELECIONADA
 matches = select_supabase(supabase, 'jogos_com_detalhes','*',[{'coluna':'season_id', 'operador':'eq','valor':id_escolhido}])
 
-#GERANDO COLUNAS PARA APRESENTAÇÃO DE DADOS
+#GERANDO COLUNAS PARA APRESENTAÇÃO DE DADOS (BIG_NUMBERS)
 col1, col2, col3, col4 = st.columns(4)
 media_total, jogos_total, gols_total = media_gols(matches)
+
+## COLUNA 1 - RODADAS DISPUTADAS
 with col1:
-    big_number_card('Rodadas Disputadas', max(matches['current_matchday'])) ## RODADAS DISPUTADAS
+    big_number_card('Rodadas Disputadas', max(matches['current_matchday']))
 
+## COLUNA 2 - QUANTIDADE DE JOGOS DISPUTADOS
 with col2:
-    big_number_card('Partidas Disputadas', jogos_total) ## QUANTIDADE DE JOGOS DISPUTADOS
+    big_number_card('Partidas Disputadas', jogos_total)
 
+## COLUNA 3 - TOTAL DE GOLS MARCADOS NA COMPETIÇÃO/TEMPORADA
 with col3:
-    big_number_card('Total de Gols Marcados', gols_total) ##TOTAL DE GOLS MARCADOS NA COMPETIÇÃO/TEMPORADA
+    big_number_card('Total de Gols Marcados', gols_total)
 
+## COLUNA 4 - MEDIA DE GOLS POR JOGO NA COMPETIÇÃO/TEMPORADA
 with col4:
-    big_number_card('Media de Gols por Jogo:', media_total) ##MEDIA DE GOLS POR JOGO NA COMPETIÇÃO/TEMPORADA
+    big_number_card('Media de Gols por Jogo:', media_total)
 
 st.markdown('---')
 
