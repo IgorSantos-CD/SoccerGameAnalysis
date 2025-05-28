54320750d1253e093ffd354f82889e92cbc4e40d
IgorSantos-CD <igor.5antos@outlook.com>
2025-05-28 11:28:43 -0300

    ajustando pacotes

--- func_streamlit/__init__.py
+++ func_streamlit/__init__.py
@@ -1,2 +1,2 @@
-from .buscar_dados import year_validate, rodadas_disputadas, media_gols
-from .apresentar import big_number_card
+from .buscar_dados import year_validate, rodadas_disputadas, media_gols, formatar_placares
+from .apresentar import big_number_card, plotar_contagem
