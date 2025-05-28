df73b3c010313126a9af851aa1edbc571ba7b0e6
IgorSantos-CD <igor.5antos@outlook.com>
2025-05-28 18:02:32 -0300

    atualizando funções

--- func_streamlit/__init__.py
+++ func_streamlit/__init__.py
@@ -1,2 +1,2 @@
-from .buscar_dados import year_validate, rodadas_disputadas, media_gols, formatar_placares
-from .apresentar import big_number_card, plotar_contagem
+from .buscar_dados import year_validate, rodadas_disputadas, media_gols, formatar_placares, formatar_resultados
+from .apresentar import big_number_card, plotar_contagem, plotar_pizza
