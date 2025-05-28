d04ef6ab72038887894aa762fb3541152d67aab2
IgorSantos-CD <igor.5antos@outlook.com>
2025-05-28 11:29:06 -0300

    função para plotar contagem

--- func_streamlit/apresentar.py
+++ func_streamlit/apresentar.py
@@ -1,11 +1,12 @@
 import streamlit as st
-
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Função para criar o quadro estilizado
 def big_number_card(title, value,color="#000000"):
     st.markdown(f"""
     <div style='
-        width: 80%;
+        width: 100%;
         height: 50%;
         border: 2px solid {color};
         border-radius: 10px;
@@ -17,7 +18,17 @@ def big_number_card(title, value,color="#000000"):
         justify-content: center;
         align-items: center;
     '>
-        <h3 style='margin-bottom:5px; color: {color}; font-size: 15px'>{title}</h3>
-        <h2 style='margin:0; color: {color}; font-size: 30px'>{value}</h2>
+        <h3 style='margin-bottom:5px; color: {color}; font-size: 13px'>{title}</h3>
+        <h2 style='margin:0; color: {color}; font-size: 25px'>{value}</h2>
     </div>
 """, unsafe_allow_html=True)
+    
+def plotar_contagem(df, col):
+    contagem = df[col].value_counts().head(5)
+    plt.figure(figsize=(12,8))
+    sns.barplot(x= contagem.index, y=contagem.values)
+    plt.xlabel(col)
+    plt.xticks(rotation=45)
+    plt.show()
+
+    
